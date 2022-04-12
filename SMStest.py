ae667aecb28bfb3195c29409209c59028ef4c637
Ali Kamel <alik87932@gmail.com>
2022-04-12 23:01:57 +0100

    ran basic sms test

--- SMStest.py
+++ SMStest.py
@@ -1,4 +1,11 @@
 from twilio.rest import Client
 
 account_sid = "AC160beabbacde45247733ce54890c6abc"
-account_auth
+auth = "b7f44ee4a5765575a73f67fef286549d"
+client = Client(account_sid, auth)
+message = client.messages.create(
+    body = "wassup dargie",
+    from_="+19896327913",
+    to="+447849222651"
+)
+print(message.sid)
